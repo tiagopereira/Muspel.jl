081f2297b4cf05ee8cdcf4fe971f9cc07f081511
edvhar <edvhar@outlook.com>
2022-08-04 22:58:30 +0200

    Added Atmosphere test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,7 @@ using Muspel
 using Test
 
 @testset "Muspel" verbose=true begin
-    # Ordered by dependency -> Should fix top tests first
-    #include("types.jl")
+    include("types.jl")
     include("read_utils.jl")
     include("lte.jl")
     include("background.jl")
