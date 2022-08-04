5f6409ac4e01b19affe808d591ffd7e666339215
edvhar <edvhar@outlook.com>
2022-08-04 22:55:13 +0200

    Update read_utils tests

--- test/read_utils.jl
+++ test/read_utils.jl
@@ -1,60 +1,74 @@
 using Muspel
 using Test
+using Transparency: const_quadratic_stark
 using Unitful
 using YAML
+import PhysicalConstants.CODATA2018: h, c_0
 
 @testset "read_utils.jl" begin
-    using Transparency: h, c_0
     @testset "read_atom" begin
-        # Type
-        @test (
-            typeof(read_atom("test_atoms/H_test_empty.yaml")) == AtomicModel{4, Float64, Int64}
+        # Single/Double precision
+        @test (typeof(read_atom("test_atoms/H_test.yaml")) == AtomicModel{4, Float64,
+                                                                          Int64})
+        @test (typeof(read_atom("test_atoms/H_test.yaml", FloatT=Float32, IntT=Int32)
+                     ) == AtomicModel{4, Float32, Int32}
         )
-        @test (typeof(read_atom("test_atoms/H_test.yaml")) == AtomicModel{4, Float64, Int64})
-        # Fields in output
-        He = read_atom("test_atoms/He_test.yaml")
+        # Simple fields
         H = read_atom("test_atoms/H_test.yaml")
+        H_yaml = YAML.load_file("test_atoms/H_test.yaml")
         H_empty = read_atom("test_atoms/H_test_empty.yaml")
-        @test He.element == :He
         @test H.element == :H
         @test H.nlevels == 4
-        @test He.nlevels == 5
         @test H.nlines == 2
-        @test H_empty.nlines == 0
+        @test H_empty.nlines == H_empty.ncontinua == 0
         @test H.ncontinua == 2
-        @test H_empty.ncontinua == 0
         @test H.Z == 1
-        @test He.Z == 2
         @test H.mass == 1.6738233791328e-27
-        @test He.mass == 6.646477321159107e-27
-        @test length(H.χ) == H.nlevels
-        @test length(H_empty.χ) == H_empty.nlevels
-        @test length(He.χ) == He.nlevels
+        @test H_empty.mass == 1.674e-27
+        @test length(H.χ) == length(H_empty.χ) == 4
         @test H.χ[1] == 0.0
-        @test H.χ[2] ≈ 1.6340148841677004e-18
-        @test length(H.g) == H.nlevels
-        @test length(H_empty.g) == H_empty.nlevels
-        @test H.g[1] == 2
-        @test H.g[2] == 8
-        @test length(H.stage) == H.nlevels
-        @test length(H_empty.stage) == H_empty.nlevels
-        @test typeof(H.label) == Vector{String}
-        # AtomicLine/AtomicContinuum tested in types.jl
+        @test all(H.χ[2:end] ≈ [1.6340148841677004e-18, 1.9366102411805722e-18,
+                                2.1786865188450865e-18])
+        @test H_empty.χ == H.χ
+        @test H.g == [2, 8, 18, 1]
+        @test H_empty.g == H.g
+        @test H.stage == [1, 1, 1, 2]
+        @test H_empty.stage == H.stage
+        @test H.label == ["H I 1S 2SE", "H I 2P 2PO", "H I 3D 2DE", "H II"]
+        @test H_empty.label == H.label
+        # Lines
+        # Test fields of H.lines one at a time. (AtomicLine contains string fields)
+        @test H_empty.lines == Vector{AtomicLine}()
         @test typeof(H.lines) == Vector{AtomicLine}
-        @test length(H.lines) == H.nlines
-        @test typeof(H_empty.lines) == Vector{AtomicLine}
-        @test length(H_empty.lines) == H_empty.nlines
-        @test typeof(H.continua) == Vector{AtomicContinuum}
-        @test length(H.continua) == H.ncontinua
-        @test typeof(H_empty.continua) == Vector{AtomicContinuum}
+        ll = Muspel.read_line.(
+                H_yaml["radiative_bound_bound"],
+                Ref(H.χ*u"J"),
+                Ref(H.g),
+                Ref(H.stage),
+                Ref(["lev1", "lev2", "lev3", "lev1_ion1"]),
+                Ref(H.label),
+                H.mass*u"kg"
+        )
+        @test all([
+            all(getfield.(H.lines, f) .== getfield.(ll, f)) for f in fieldnames(AtomicLine)
+        ])
+        # Continua
+        @test H_empty.continua == Vector{AtomicContinuum}()
         @test length(H_empty.continua) == H_empty.ncontinua
+        @test all(
+            H.continua .== Muspel.read_continuum.(H_yaml["radiative_bound_free"],
+                                                  Ref(H.χ*u"J"),
+                                                  Ref(H.stage),
+                                                  Ref(["lev1", "lev2", "lev3", "lev1_ion1"])
+            )
+        )
     end
     @testset "read_continuum" begin
         tr = ["lev1", "lev1_ion1"]
         χ = [10., 10.5, 11.0] * u"aJ"
         stage = [1, 1, 2]
         level_ids = ["lev1", "lev2", "lev1_ion1"]
-        # cross_section
+        ### cross_section:
         cs = Dict("value"=>[[1e2, 1e-20],
                             [3e2, 3e-20],
                             [2e2, 2e-20]],
@@ -62,39 +76,35 @@ using YAML
         )
         cont = Dict("cross_section"=>cs, "transition"=> tr)
         continuum = Muspel.read_continuum(cont, χ, stage, level_ids)
-        ## Sorted wavelengths and crossections
+        @test continuum.nλ == 3
+        @test length(continuum.λ) == 3
+        @test length(continuum.σ) == 3
+        # Sorted wavelengths and crossections
         @test all((continuum.λ[2:end] .- continuum.λ[1:end-1]) .> 0)
         for i in 1:3
             idx = findall(continuum.λ .== cs["value"][i][1])
             @test continuum.σ[idx] == [cs["value"][i][2]]
         end
-        ## Lengths
-        @test continuum.nλ == 3
-        @test length(continuum.λ) == 3
-        @test length(continuum.σ) == 3
-        ## λedge from previous implementation
+        # λedge from previous implementation
         @test continuum.λedge ≈ 198.64458571489286
-        ## indexes
+        # indexes
         @test (continuum.up, continuum.lo) == (3, 1)
-        # cross_section_hydrogenic
+        ### cross_section_hydrogenic:
         cs2 = Dict("nλ"=>3,
                   "λ_min"=>Dict("value"=>1980., "unit"=>"Å"),
                   "σ_peak"=>Dict("value"=>1e-20, "unit"=>"m^2"),
         )
         cont2 = Dict("cross_section_hydrogenic"=>cs2, "transition"=> tr)
         continuum = Muspel.read_continuum(cont2, χ, stage, level_ids)
-        ## λmin < λedge
+        @test continuum.nλ == 3
+        # λmin < λedge
         @test_throws AssertionError Muspel.read_continuum(
                                         cont2, [10.0, 11.0, 12.0] * u"aJ", stage, level_ids)
-        ## Lengths
-        @test continuum.nλ == 3
-        @test length(continuum.λ) == 3
-        @test length(continuum.σ) == 3
-        ## Values from previous implementation
+        # Values from previous implementation
         @test continuum.λedge ≈ 198.64458571489286
         @test all(continuum.λ .≈ [198.0, 198.32229285744643, 198.64458571489286])
         @test all(continuum.σ .≈ [9.911504012980945e-21, 9.955692900228031e-21, 1.0e-20])
-        # Missing keyword
+        ### Missing keyword:
         @test_throws ErrorException Muspel.read_continuum(
                             Dict("transition"=> ["lev1", "lev1_ion1"]), χ, stage, level_ids)
     end
@@ -110,7 +120,7 @@ using YAML
         dline = data["radiative_bound_bound"][4]
         line = Muspel.read_line(
                     dline, χ, g, stage, level_ids, label, mass, FloatT=Float64, IntT=Int64)
-        # General
+        # General:
         @test line.χup == 10.5
         @test line.χlo == 10.0
         @test line.gup == 8
@@ -125,12 +135,12 @@ using YAML
         @test all(line.γ_vdW_const .== [2.3e-15])
         @test all(line.γ_vdW_exp .== [0.0])
         @test line.γ_quad_stark_const == 0
-        # when "data" in keys # Using no 4
+        # when "data" in keys:
         @test line.nλ == 3
         @test line.λ == [100.0, 200.0, 300.0]
         @test line.PRD == false
         @test line.Voigt == false
-        # Else when type = RH # Use no 3 and no 2
+        # Else when type = RH:
         dline = data["radiative_bound_bound"][3]
         line = Muspel.read_line(
                     dline, χ, g, stage, level_ids, label, mass, FloatT=Float64, IntT=Int64)
@@ -147,7 +157,7 @@ using YAML
                         397.46131378793945, 399.2794199002346])
         @test line.PRD == true
         @test line.Voigt == true
-        # Else when type = MULTI # Use no 1
+        # Else when type = MULTI:
         dline = data["radiative_bound_bound"][1]
         line = Muspel.read_line(
                     dline, χ, g, stage, level_ids, label, mass, FloatT=Float64, IntT=Int64)
@@ -156,7 +166,7 @@ using YAML
                         397.3860246364557, 397.43463712667443, 403.7840001532265])
         @test line.PRD == false
         @test line.Voigt == true
-        # Else unrecognized types
+        # Else unrecognized types:
         dline_u = dline
         dline_u["type_profile"] = "Something"
         @test_throws ErrorException Muspel.read_line(
@@ -167,13 +177,93 @@ using YAML
                      dline, χ, g, stage, level_ids, label, mass, FloatT=Float64, IntT=Int64)
     end
     @testset "calc_λline_RH" begin
-        ##
+        # Wavelength values are against previous implementation
+        λ0 = 500.0u"nm"
+        nλ = 4
+        qcore = 15.0
+        qwing = 600.0
+        vξ = 2.5u"km/s"
+        # Symm/asymm:
+        wav = Muspel.calc_λline_RH(λ0, 5, qcore, qwing, vξ) # Does not include λ0
+        @test all(
+            ustrip(wav) .≈ [497.4950614115939, 499.99679212558004,
+                            500.00320787441996, 502.5049385884061]
+        )
+        @test all(unit.(wav) .== u"nm")
+        wav = Muspel.calc_λline_RH(λ0, nλ, qcore, qwing, vξ; asymm=false)
+        @test all(
+            ustrip(wav) .≈ [500.0030549856672, 500.02105292375967,
+                            500.2166461742894, 502.5047856996533]
+        )
+        @test all(unit.(wav) .== u"nm")
+        # qwing <= 2 * qcore:
+        wav = Muspel.calc_λline_RH(λ0, nλ, qcore, 20.0, vξ; asymm=false)
+        @test all(
+            ustrip(wav) .≈ [500.05559401586635, 500.08339102379955,
+                            500.1111880317327, 500.1389850396659]
+        )
+        # Units:
+        wav = Muspel.calc_λline_RH(100.0u"Å", nλ, qcore, qwing, vξ; asymm=false)
+        @test all(
+            ustrip(wav) .≈ [100.00061099713344, 100.00421058475195,
+                            100.04332923485788, 100.50095713993066]
+        )
+        @test all(unit.(wav) .== u"Å")
+        # Single precision:
+        @test_throws MethodError Muspel.calc_λline_RH(1f0u"Å", nλ, qcore, qwing, vξ)
+        wav = Muspel.calc_λline_RH(1f2u"Å", nλ, convert(Float32, qcore),
+                                   convert(Float32, qwing), 2.5f3u"m/s"; asymm=false)
+        @test eltype(ustrip(wav)) == Float32
+        @test all(
+            ustrip(wav) .≈ [100.00061099713344, 100.00421058475195,
+                            100.04332923485788, 100.50095713993066]
+        )
     end
     @testset "calc_λline_MULTI" begin
-        ##
+        # Wavelength values are against previous implementation
+        λ0 = 500.0u"nm"
+        nλ = 4
+        q0 = 3.0
+        qmax = 600.0
+        vξ = 8.0u"km/s"
+        # Symm/asymm, (qmax > q0) and (qmax >= 0) and (q0 >= 0):
+        wav = Muspel.calc_λline_MULTI(λ0, nλ, q0, qmax, vξ)
+        @test all(
+            ustrip(wav) .≈ [492.08474595750886, 499.971569413828,
+                            500.02843381973275, 508.17405286233975]
+        )
+        @test all(unit.(wav) .== u"nm")
+        wav = Muspel.calc_λline_MULTI(λ0, 5, q0, qmax, vξ) # Includes λ0
+        @test all(
+            ustrip(wav) .≈ [492.08474595750886, 499.9523432985373, 500.0,
+                            500.0476657878395, 508.17405286233975]
+        )
+        wav = Muspel.calc_λline_MULTI(λ0, nλ, q0, qmax, vξ; asymm=false)
+        @test all(
+            ustrip(wav) .≈ [500.0, 500.0284296341159, 500.120260419649, 508.17404850397065]
+        )
+        # (qmax <= q0) linear spacing:
+        wav = Muspel.calc_λline_MULTI(λ0, nλ, q0, 1.0, vξ) # qmax > 0
+        @test all(
+            ustrip(wav) .≈ [499.98665779223063, 499.99555251829133,
+                            500.0044475608306, 500.01334291986547]
+        )
+        dw = wav[2:end] - wav[1:end-1]
+        @test all(isapprox.(dw[1], dw, rtol=1e-3))
+        # None of the above:
+        @test all(Muspel.calc_λline_MULTI(λ0, nλ, -1.0, 2.0, vξ) .== 500.0u"nm")
+        # Single precision:
+        @test_throws MethodError Muspel.calc_λline_MULTI(λ0, nλ, 1f0, qmax, vξ)
+        wav = Muspel.calc_λline_MULTI(5f3u"Å", nλ, 3.0f1, 6.0f2, 8.0f3u"m/s")
+        @test eltype(ustrip(wav)) == Float32
+        @test all(
+            ustrip(wav .|> u"nm") .≈ [492.08474595750886, 499.971569413828,
+                                      500.02843381973275, 508.17405286233975]
+        )
     end
     @testset "_assign_unit" begin
-        ##
+        @test Muspel._assign_unit(Dict("value"=>1.0, "unit"=>"km / s")) == 1.0u"km/s"
+        @test_throws MethodError Muspel._assign_unit(Dict("value"=>"m", "unit"=>1.0))
     end
     @testset "_read_transition" begin
         # Check a sample
@@ -189,13 +279,52 @@ using YAML
         w, up, lo =  Muspel._read_transition(data, χ, level_ids)
         @test (up, lo) == (4, 2)
     end
+    mass = 1.67e-27u"kg"
+    χup = 1.63u"aJ"
+    χlo = 0.0u"aJ"
+    χ∞ = 2.18u"aJ"
+    Z = 1
     @testset "_read_vdW_single" begin
-        ##
+        data_1a = Dict("type"=>"Unsold")
+        data_1b = Dict("type"=>"unsold", "h_coefficient"=>1.)
+        data_1c = Dict("type"=>"unsold", "he_coefficient"=>1.)
+        data_1d = Dict("type"=>"unsold", "h_coefficient"=>0.4, "he_coefficient"=>0.5)
+        data_2 = Dict("type"=>"ABO", "α"=>Dict("value"=>2.0), "σ"=>Dict("value"=>3.0))
+        data_3 = Dict(
+            "type"=>"deridder_rensbergen",
+            "h"=>Dict("α"=>Dict("value"=>2.0, "unit"=>"1e-8*cm^3/s"),"β"=>1.2)
+        )
+        @test_throws AssertionError Muspel._read_vdW_single(Dict(), mass, χup, χlo, χ∞, Z)
+        @test_throws ErrorException Muspel._read_vdW_single(
+                                                   Dict("type"=>"s"), mass, χup, χlo, χ∞, Z)
+        @test Muspel._read_vdW_single(data_1a, mass, χup, χlo, χ∞, Z) == (0.0, 0.3)
+        @test all(Muspel._read_vdW_single(
+            data_1b, mass, χup, χlo, χ∞, Z) .≈ (1.4630219520027063e-15, 0.3))
+        @test all(Muspel._read_vdW_single(
+            data_1c, mass, χup, χlo, χ∞, Z) .≈ (1.242488840030318e-16, 0.3))
+        @test all(Muspel._read_vdW_single(
+            data_1d, mass, χup, χlo, χ∞, Z) .≈ (6.473332228025985e-16, 0.3))
+        @test all(Muspel._read_vdW_single(
+            data_2, mass, χup, χlo, χ∞, Z) .≈ (1.043135275447453e-14, -0.5))
+        @test all(Muspel._read_vdW_single(
+            data_3, mass, χup, χlo, χ∞, Z) .≈ (4.588496830823747e-14, 1.2))
     end
     @testset "_read_vdW" begin
-        ##
+        d = Dict("type"=>"Unsold")
+        @test Muspel._read_vdW(
+                Dict("broadening_vanderwaals"=>d), mass, χup, χlo, χ∞, Z) == ([0.0], [0.3])
+        @test Muspel._read_vdW(
+            Dict("broadening_vanderwaals"=>[d,d]), mass, χup, χlo, χ∞, Z) == ([0.0, 0.0],
+                                                                               [0.3, 0.3])
+        @test Muspel._read_vdW(Dict("something"=>1), mass, χup, χlo, χ∞, Z) == ([], [])
     end
     @testset "_read_quadratic_stark" begin
-        ##
+        data = Dict()
+        @test Muspel._read_quadratic_stark(data, mass, χup, χlo, χ∞, Z) == 0.0
+        data["broadening_stark"] = Dict("coefficient" => 1.0)
+        @test Muspel._read_quadratic_stark(data, mass, χup, χlo, χ∞, Z
+            ) == ustrip(const_quadratic_stark(mass, χup, χlo, χ∞, Z) |> u"m^3 / s")
+        data["C_4"] = Dict("unit"=>"m^3/s", "value"=>2.0)
+        @test Muspel._read_quadratic_stark(data, mass, χup, χlo, χ∞, Z) == 2.0
     end
 end
