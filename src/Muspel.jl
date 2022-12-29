4b104840507fe3410c4190b83c3f5d7d33421604
Tiago Pereira <1014003+tiagopereira@users.noreply.github.com>
2022-12-29 20:10:44 +0100

    Added missing export

--- src/Muspel.jl
+++ src/Muspel.jl
@@ -9,7 +9,7 @@ export AbstractExtinctionItp, ExtinctionItpLTE, ExtinctionItpNLTE
 export α_cont, α_cont_no_itp, get_σ_itp
 export piecewise_1D_nn, piecewise_1D_linear, piecewise_1D_linear2!, feautrier
 export read_atmos_rh, read_pops_rh
-export read_atmos_multi3d, read_pops_multi3d
+export read_atmos_hpops_multi3d, read_atmos_multi3d, read_pops_multi3d
 export read_atmos_multi3d_double, read_pops_multi3d_double
 export doppler_width, damping, calc_broadening, create_voigt_itp
 export blackbody_λ
