20be13cc3565c320ddca5c6791e0e9b13ba03704
Tiago Pereira <1014003+tiagopereira@users.noreply.github.com>
2023-08-10 15:53:46 +0200

    Added scripts for int from pops, RH and Multi3D

--- scripts/int_from_pops_rh.jl
+++ scripts/int_from_pops_rh.jl
@@ -4,19 +4,18 @@ using HDF5
 using ProgressMeter
 using Base.Threads
 
-
 """
 Sample script to calculate Ca II 854.2 nm disk-centre intensity from
-a RH atmosphere and populations.
+an RH atmosphere and populations.
+
+Assumes an atom with a similar structure to CaII_PRD.yaml is used.
 """
-function do_work_rh(atmos_file, aux_file, atom_file)
-    # read atomic data
+function calc_rh_8542(atmos_file, aux_file, atom_file)
     ca = read_atom(atom_file)
     my_line = ca.lines[5]  # Assumes CaII_PRD.yaml, index 5 for 854.2
 
-    # read atmosphere from RH
     atmos = read_atmos_rh(atmos_file)
-    ca_pops = h5read(aux_file, "atom_CA/populations")
+    ca_pops = read_pops_rh(aux_file, "CA")
     n_u = ca_pops[:, :, :, 5]
     n_l = ca_pops[:, :, :, 3]
 
@@ -41,12 +40,10 @@ function do_work_rh(atmos_file, aux_file, atom_file)
     σ_itp = get_σ_itp(atmos, my_line.λ0, atom_files)
 
     a = LinRange(1f-4, 1f1, 20000)
-    v = LinRange(-5f2, 5f2, 5000)
+    v = LinRange(0f0, 5f2, 2500)
     voigt_itp = create_voigt_itp(a, v)
 
-    # result
     intensity = Array{Float32, 3}(undef, my_line.nλ, atmos.nx, atmos.ny)
-
     p = ProgressMeter.Progress(atmos.nx)
 
     Threads.@threads for i in 1:atmos.nx
@@ -57,24 +54,28 @@ function do_work_rh(atmos_file, aux_file, atom_file)
         end
         ProgressMeter.next!(p)
     end
+
     return intensity
 end
 
 
 """
-Sample script to calculate Ca II 854.2 nm disk-centre intensity from
-a Multi3D atmosphere and populations.
-"""
-function do_work_multi3d(mesh_file, atmos_file, pops_file, atom_file)
-    # read atomic data
-    ca_atom = read_atom(atom_file)
-    my_line = ca_atom.lines[5]  # Assumes CaII_run.yaml, index 5 for 854.2
+Sample script to calculate Hα disk-centre intensity from a RH atmosphere and
+populations.
 
-    # read atmosphere and populations from Multi3D
-    atmos = read_atmos_multi3d(mesh_file, atmos_file)
-    pops = read_pops_multi3d(pops_file, atmos.nx, atmos.ny, atmos.nz, ca_atom.nlevels)
-    n_u = pops[:, :, :, 5]
-    n_l = pops[:, :, :, 3]
+The difference from the previous function is that the NLTE hydrogen
+populations are read at the same time as the atmosphere, and used to get the
+densities of H I and H II that go into the background opacity calculations.
+
+Assumes an atom with a similar structure to H_6.yaml is used.
+"""
+function calc_rh_hα(atmos_file, aux_file, atom_file)
+    h_atom = read_atom(atom_file)
+    my_line = h_atom.lines[5]  #  index 5 for Halpha
+
+    atmos, h_pops = read_atmos_hpops_rh(atmos_file)
+    n_u = h_pops[:, :, :, 3]
+    n_l = h_pops[:, :, :, 2]
 
     # Continuum opacity structures
     bckgr_atoms = [
@@ -96,13 +97,11 @@ function do_work_multi3d(mesh_file, atmos_file, pops_file, atom_file)
     atom_files = [joinpath(AtomicData.get_atom_dir(), a) for a in bckgr_atoms]
     σ_itp = get_σ_itp(atmos, my_line.λ0, atom_files)
 
-    a = LinRange(1f-4, 1f1, 20000)
-    v = LinRange(-5f2, 5f2, 5000)
+    a = LinRange(1f-4, 1.5f1, 20000)
+    v = LinRange(0f0, 5f2, 5000)
     voigt_itp = create_voigt_itp(a, v)
 
-    # result
     intensity = Array{Float32, 3}(undef, my_line.nλ, atmos.nx, atmos.ny)
-
     p = ProgressMeter.Progress(atmos.nx)
 
     Threads.@threads for i in 1:atmos.nx
@@ -113,5 +112,6 @@ function do_work_multi3d(mesh_file, atmos_file, pops_file, atom_file)
         end
         ProgressMeter.next!(p)
     end
+
     return intensity
 end
